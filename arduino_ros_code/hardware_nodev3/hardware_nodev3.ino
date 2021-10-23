0c5f867b4b9fa1f73a05b0a5b6021957814a646a
Rjames13 <rnj13@case.edu>
2021-10-23 14:06:58 -0400

    add launch file

--- arduino_ros_code/hardware_nodev3/hardware_nodev3.ino
+++ arduino_ros_code/hardware_nodev3/hardware_nodev3.ino
@@ -25,6 +25,8 @@ Sabertooth back(128, Serial1);
 Sabertooth front(129, Serial1);
 
 
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
 
 
 // ros vars
@@ -43,11 +45,13 @@ ros::Publisher currentData("currentData", &str_msg);
 const long TIMEOUT = 1000;
 unsigned long last_time, dt;
 
+int use_gyro = 1;
+
 String debug_str;
 
 
 void setup() {
-  // put your setup code here, to run once:
+ //Ros setup
   Serial1.begin(9600);
   portROS.begin(57600);
 
@@ -61,6 +65,24 @@ void setup() {
   nh.advertise(currentData);
 
   move(0, 0);
+
+  //gyro setup
+   int sensor_check = 0;
+ while((!bno.begin())||sensor_check<10)
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+
+    nh.logerror("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    sensor_check++;
+    delay(1000);
+  }
+  //if(sensor_check >= 10 ){
+    //use_gyro = 0;
+    //nh.logerror("Gyro not setup!!! Continueing with out");
+  //}
+  delay(1000);
+
+  bno.setExtCrystalUse(true);
 }
 
 void loop() {
@@ -76,7 +98,9 @@ void loop() {
       +String(analogRead(A3),HEX));
     str_msg.data = msg.c_str();
       currentData.publish( &str_msg);
-
+   if(use_gyro == 1){
+    publishSensorData();
+}
 
 
   Serial1.flush();
@@ -117,4 +141,116 @@ void move(int left, int right) {
 
 void stop() {
   move(0, 0);
+}
+
+
+
+void publishSensorData(){
+
+//send sensor data packet
+       String msg;
+       //could add VECTOR_ACCELEROMETER, VECTOR_MAGNETOMETER,VECTOR_GRAVITY...
+       sensors_event_t orientationData , angVelocityData , linearAccelData, magnetometerData, accelerometerData, gravityData;
+       bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+       bno.getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+       bno.getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+       bno.getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
+       bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+       bno.getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
+
+       msg += (printEvent(&orientationData));
+       msg +=(printEvent(&angVelocityData));
+       msg +=(printEvent(&linearAccelData));
+       msg +=(printEvent(&magnetometerData));
+       msg +=(printEvent(&accelerometerData));
+       msg +=(printEvent(&gravityData));
+
+      int8_t boardTemp = bno.getTemp();
+
+      msg +=(F("temperature: "));
+      msg +=(boardTemp);
+
+      uint8_t system, gyro, accel, mag = 0;
+      bno.getCalibration(&system, &gyro, &accel, &mag);
+
+      msg +=("Calibration: Sys=");
+      msg +=(String(system));
+      msg +=(" Gyro=");
+      msg +=(String(gyro,HEX));
+      msg +=(" Accel=");
+      msg +=(String(accel,HEX));
+      msg +=(" Mag=");
+      msg +=String(mag,HEX);
+
+      msg += ("--");
+
+
+
+      str_msg.data = msg.c_str();
+      sensorData.publish( &str_msg);
+
+}
+
+
+
+
+// want to change to publish to diffrent topics
+String printEvent(sensors_event_t* event) {
+
+  String msg;
+  double x = -1000000, y = -1000000 , z = -1000000; //dumb values, easy to spot problem
+  if (event->type == SENSOR_TYPE_ACCELEROMETER) {
+    msg += "Accl:" ;
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else if (event->type == SENSOR_TYPE_ORIENTATION) {
+     msg += "Orient:";
+    x = event->orientation.x;
+    y = event->orientation.y;
+    z = event->orientation.z;
+  }
+  else if (event->type == SENSOR_TYPE_MAGNETIC_FIELD) {
+     msg += "Mag:";
+    x = event->magnetic.x;
+    y = event->magnetic.y;
+    z = event->magnetic.z;
+  }
+  else if (event->type == SENSOR_TYPE_GYROSCOPE) {
+     msg += "Gyro:";
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+  else if (event->type == SENSOR_TYPE_ROTATION_VECTOR) {
+     msg += "Rot:";
+    x = event->gyro.x;
+    y = event->gyro.y;
+    z = event->gyro.z;
+  }
+  else if (event->type == SENSOR_TYPE_LINEAR_ACCELERATION) {
+     msg += "Linear:";
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else if (event->type == SENSOR_TYPE_GRAVITY) {
+     msg += "Gravity:";
+    x = event->acceleration.x;
+    y = event->acceleration.y;
+    z = event->acceleration.z;
+  }
+  else {
+    nh.logwarn("Unk:");
+  }
+
+   msg += "\tx= ";
+   msg += x;
+   msg += " |\ty= ";
+   msg += y;
+   msg += " |\tz= ";
+   msg += z;
+
+   return msg;
 }
